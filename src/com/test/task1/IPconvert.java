b1867dea418e9b10aa4afaeaf4df004f64ab8f40
alexluhin <alexsim2009@rambler.ru>
2022-06-08 23:59:17 +0300

    modify inputs

--- src/com/test/task1/IPconvert.java
+++ src/com/test/task1/IPconvert.java
@@ -14,22 +14,7 @@ public class IPconvert
         System.out.print("Input IP address: ");
         String ip_str = in.next();
         in.close();
-/*
-        for (String str : args)
-        {
-            System.out.println("arg = " + str);
-            if (str.startsWith("-ip="))
-            {
-                String[] st = str.split("=");
-                if(st[1].length() > 0)
-                {
-                    ip_str = st[1];
-                    break;
-                }
-            }
 
-        }
-*/
         long longip = cl.ipToLong(ip_str);
 
         System.out.println("ip address : " + ip_str + "\nlongip : " + longip);
